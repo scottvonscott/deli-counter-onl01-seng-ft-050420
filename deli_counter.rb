be6e83bdec3c3e63c09f097ac89e16668aa0f1d6
Scott  Keplinger <skeplin17@gmail.com>
2020-05-07 18:37:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,7 @@ def take_a_number(katz_deli, new_customer)
   katz_deli << new_customer
   puts "Welcome, #{new_customer}. You are number #{katz_deli.index(new_customer) +1} in line."
 end
+
+def now_serving(katz_deli)
+
+end
