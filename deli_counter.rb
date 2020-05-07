45c62fb789ca5a31ee4c34b06575b7846e2ce3ab
Scott  Keplinger <skeplin17@gmail.com>
2020-05-07 17:54:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,8 @@ def line(katz_deli)
   counter = 0
   if katz_deli.length != 0
     katz_deli.each do |customer|
-      counter += 1
       puts "The line is currently: #{counter} #{customer}."
+      counter += 1
     end
   else
       puts "The line is currently empty."
