a2ef0ba4188a99f0e260f952bee86f91dd4fdfd3
Scott  Keplinger <skeplin17@gmail.com>
2020-05-07 18:21:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,7 +24,7 @@ def line(katz_deli)
   current_line = "The line is currently: "
   if katz_deli.length != 0
     katz_deli.each do |customer|
-      current_line << "#{katz_deli.index(customer) + 1}. #{customer}"
+      current_line << " #{katz_deli.index(customer) + 1}. #{customer}"
     end
     puts current_line
   else
